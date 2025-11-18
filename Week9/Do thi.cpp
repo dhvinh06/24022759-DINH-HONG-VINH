831588456f4f2ec5345b76c6e0dbb2d42181e99f
dhvinh06 <vinhhtm@gmail.com>
2025-11-18 15:59:04 +0700

    Create Do thi.cpp

---
+++ Week9/Do thi.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring> // Dùng cho memset
+
+using namespace std;
+
+const int MAX = 1001;
+
+vector<int> adj[MAX];
+bool visited[MAX];
+
+void addEdge(int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void DFS(int u) {
+    cout << u << " ";
+    visited[u] = true;
+    for (int v : adj[u]) {
+        if (!visited[v]) {
+            DFS(v);
+        }
+    }
+}//de quy
+
+void BFS(int start) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        cout << u << " ";
+
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                q.push(v);
+                visited[v] = true;
+            }
+        }
+    }
+}
+
